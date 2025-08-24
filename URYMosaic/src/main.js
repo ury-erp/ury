053de60744d0aa6221380f84ccbfd954dbe21d92
Voltass <57225312+Voltass@users.noreply.github.com>
2025-08-24 13:36:21 +0100

    Update main.js
    # change: import $auth from "@/services/auth.js";
    #    to:  import $auth from "@/auth.js";

--- URYMosaic/src/main.js
+++ URYMosaic/src/main.js
@@ -6,7 +6,7 @@ import "./index.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import $auth from "@/services/auth.js";   // <-- new
+import $auth from "@/auth.js";   // <-- new
 
 const app = createApp(App);
 
