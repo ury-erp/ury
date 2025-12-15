58568c3ae8739d0e426f637bc0197bfc8cd9d6c1
Someonegreat <patrickad@carbonitesolutions.com>
2025-12-15 20:28:18 +0000

    ury pos display test

--- pos/src/main.tsx
+++ pos/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import './lib/qz-init';
+import './lib/pos-display';
 import { setupKotListener } from './lib/kot-listener';
 
 createRoot(document.getElementById('root')!).render(
