35fa24d74c13147461fdddb4f088fab2cdcc7eb5
Someonegreat <patrickad@carbonitesolutions.com>
2025-12-16 06:49:18 +0000

    fix: POS Display not showing

--- pos/src/main.tsx
+++ pos/src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import './lib/qz-init';
-import './lib/pos-display';
 import { setupKotListener } from './lib/kot-listener';
 
 createRoot(document.getElementById('root')!).render(
