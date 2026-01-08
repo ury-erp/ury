d50fa3a65e55d08a3ad50bd667e8fc3f68391b5b
vyshnav mv <vyshnavmv15@gmail.com>
2026-01-08 04:37:53 -0800

    Fix: extend delete_custom_fields() to remove URY roles

--- ury/setup.py
+++ ury/setup.py
@@ -369,36 +369,41 @@ def delete_custom_fields(custom_fields):
     removed = 0
     for doctype, fields in custom_fields.items():
         fieldnames = [field["fieldname"] for field in fields]
-
         deleted = frappe.db.delete(
             "Custom Field",
-            {
-                "dt": doctype,
-                "fieldname": ("in", fieldnames),
-            },
+            {"dt": doctype, "fieldname": ("in", fieldnames)}
         )
         removed += deleted or 0
-
+        
     extra_deleted = frappe.db.delete(
         "Custom Field",
-        {
-            "dt": ("like", "%URY%"),
-        },
+        {"dt": ("like", "%URY%")}
     )
     removed += extra_deleted or 0
+
     linked_deleted = frappe.db.delete(
         "Custom Field",
-        {
-            "options": ("like", "%URY%"),
-        },
+        {"options": ("like", "%URY%")}
     )
     removed += linked_deleted or 0
 
     frappe.db.commit()
-    print(f"Deleted {removed} URY-related custom fields")
-    
+    print(f"Deleted {removed} URY custom fields")
+    roles = frappe.get_all("Role", filters={"name": ("like", "URY%")})
+    for r in roles:
+        frappe.delete_doc("Role", r.name, force=1, ignore_permissions=True)
+    frappe.db.commit()
+
+    frappe.db.delete("Has Role", {"role": ("like", "URY%")})
+    frappe.db.delete("User Permission", {"allow": ("like", "URY%")})
+    frappe.db.commit()
+
     for doctype in custom_fields.keys():
         frappe.clear_cache(doctype=doctype)
+
+        
+    
+
 
  
     
