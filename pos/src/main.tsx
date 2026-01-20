27ba3a59fa3bb31c2c6f0b42e86e6a4eaa4b1661
vyshnav mv <vyshnavmv15@gmail.com>
2026-01-19 23:18:29 -0800

    fix: pwa application for v2 , that sw directly add in ury

--- pos/src/main.tsx
+++ pos/src/main.tsx
@@ -8,3 +8,16 @@ createRoot(document.getElementById('root')!).render(
     <App />
   </StrictMode>,
 )
+// Register Service Worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js', { scope: '/' }).then(
+      (registration) => {
+        console.log('SW registered: ', registration);
+      },
+      (registrationError) => {
+        console.log('SW registration failed: ', registrationError);
+      }
+    );
+  });
+}
