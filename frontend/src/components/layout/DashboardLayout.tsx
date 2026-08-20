954097053b24593a9f7e9d3f83cf713e2bbb9337
Ameenlakkal <ameenahsan@tridz.com>
2026-08-20 07:53:08 +0000

    style(dashboard): extend sidebar separator to viewport bottom

--- frontend/src/components/layout/DashboardLayout.tsx
+++ frontend/src/components/layout/DashboardLayout.tsx
@@ -12,11 +12,13 @@ export const DashboardLayout: React.FC = () => {
         <Header />
         <div className="flex flex-1 min-h-0 overflow-hidden">
           <Sidebar />
-          <main className="flex-1 p-6 overflow-y-auto min-w-0">
-            <Outlet />
-          </main>
+          <div className="flex-1 flex flex-col min-w-0 overflow-hidden">
+            <main className="flex-1 p-6 overflow-y-auto min-w-0">
+              <Outlet />
+            </main>
+            <Footer />
+          </div>
         </div>
-        <Footer />
       </div>
     </BranchProvider>
   );
