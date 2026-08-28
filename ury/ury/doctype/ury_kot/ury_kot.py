c5881f08509810bc108e54b744f6a285d3f84ee7
swafa-as <swafa@tridz.com>
2026-08-28 08:29:06 +0000

    feat: allow KOT printing for cashier role updates during existing order modifications

--- ury/ury/doctype/ury_kot/ury_kot.py
+++ ury/ury/doctype/ury_kot/ury_kot.py
@@ -34,6 +34,14 @@ class URYKOT(Document):
             order_by="idx"
         )
     
+        is_cashier_update = False
+        if self.type != "New Order" and self.pos_profile and self.table_takeaway == 0:
+            user_roles = frappe.get_roles()
+            pos_profile_doc = frappe.get_doc("POS Profile", self.pos_profile)
+            if any(role.role in user_roles for role in pos_profile_doc.role_allowed_for_billing):
+                is_cashier_update = True
+                frappe.log_error(message="is_cashier_update True", title="Print Fail")
+
         pos_print_flag = True
         if self.production:
             production_unit_printers = frappe.get_all(
@@ -72,7 +80,7 @@ class URYKOT(Document):
                             pos_print_flag = False
                             print_kot(printer.printer, printer.custom_kot_print_format)
 
-                    if pos_print_flag == True:
+                    if pos_print_flag == True or is_cashier_update:
                         if pos_kot_printers:
                             for printer in pos_kot_printers:
                                 print_kot(printer.printer, printer.custom_kot_print_format)
