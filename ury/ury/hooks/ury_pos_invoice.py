7168eded899ef6a0ba11244d88f80f0073ff12cd
dev3-trojanitsolutions <dev3@trojanitsolutions.com>
2026-09-09 17:42:30 +0530

    fix: resolve POS customer creation and payment issues

--- ury/ury/hooks/ury_pos_invoice.py
+++ ury/ury/hooks/ury_pos_invoice.py
@@ -1,6 +1,5 @@
 import frappe
-from datetime import datetime
-from frappe.utils import now_datetime, get_time,now
+from frappe.utils import now_datetime, time_diff_in_seconds
 from ury.ury.doctype.ury_order.ury_order import release_merge_cluster_tables
 
 
@@ -88,21 +87,10 @@ def validate_customer(doc, method):
 
 def calculate_and_set_times(doc, method):
     doc.arrived_time = doc.creation
-
-    current_time_str = now()
-    
-    current_time = datetime.strptime(current_time_str, "%Y-%m-%d %H:%M:%S.%f")
-    
-    creation_time = datetime.strptime(doc.creation, "%Y-%m-%d %H:%M:%S.%f")
-    
-    time_difference = current_time - creation_time
-    
-    total_seconds = int(time_difference.total_seconds())
+    total_seconds = int(time_diff_in_seconds(now_datetime(), doc.creation))
     hours, remainder = divmod(total_seconds, 3600)
     minutes, seconds = divmod(remainder, 60)
-    
-    formatted_spend_time = f"{hours:02d}:{minutes:02d}:{seconds:02d}"
-    doc.total_spend_time = formatted_spend_time
+    doc.total_spend_time = f"{hours:02d}:{minutes:02d}:{seconds:02d}"
 
 
 def validate_invoice_print(doc, method):
